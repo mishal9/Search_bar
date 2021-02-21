750ff905f3715cb3e4195305a9c09081606b1a5a
Mishal Shah <mshah31@asu.edu>
2021-02-20 19:49:01 -0700

    Initial commit

---
+++ client/src/HomePage/HomePage.jsx
@@ -0,0 +1,77 @@
+import React, {useEffect, useState} from 'react';
+import { Link } from 'react-router-dom';
+
+function HomePage()  {
+
+    const [artist, setArtist] = useState('')
+    const [pager, setPager] = useState({})
+    const [pageOfItems, setPageOfItems] = useState([])
+
+    
+
+    function changeHandler(event) {
+        setArtist(event.target.value)
+    };
+
+    function submitHandler() {        
+        const params = new URLSearchParams(location.search);
+        const page = parseInt(params.get('page')) || 1;
+        
+        fetch(`http://127.0.0.1:5000/api/query?artist=${artist}&page=${page}`, { method: 'GET' })
+            .then(response => response.json())
+            .then(({pager, pageOfItems}) => {
+                setPageOfItems(pageOfItems);
+                setPager(pager);
+        });
+    };
+
+    return (            
+        <div className="card text-center m-3">
+            
+            <div className="card-body">
+            <input id="name" 
+                    type="text" 
+                    autoComplete="off"
+                    placeholder="Search an artist" 
+                    onChange={changeHandler} 
+                    name="artist" 
+                    required>
+            </input>
+                    
+            <button type="submit" onClick={submitHandler}>SEARCH</button>
+                    
+            <br/>
+            <br/>
+                {pageOfItems && pageOfItems.map(item =>
+                    <div key={item.id}>{item.artistName}
+                    </div>
+                )}
+            </div>
+            <div className="card-footer pb-0 pt-3">
+                {pager.pages && pager.pages.length &&
+                    <ul className="pagination">
+                        <li className={`page-item first-item ${pager.currentPage === 1 ? 'disabled' : ''}`}>
+                            <Link onClick={submitHandler} to={{ search: `?page=1` }} className="page-link">First</Link>
+                        </li>
+                        <li className={`page-item previous-item ${pager.currentPage === 1 ? 'disabled' : ''}`}>
+                            <Link onClick={submitHandler} to={{ search: `?page=${pager.currentPage - 1}` }} className="page-link">Previous</Link>
+                        </li>
+                        {pager.pages.map(page =>
+                            <li key={page} className={`page-item number-item ${pager.currentPage === page ? 'active' : ''}`}>
+                                <Link onClick={submitHandler} to={{ search: `?page=${page}` }} className="page-link">{page}</Link>
+                            </li>
+                        )}
+                        <li className={`page-item next-item ${pager.currentPage === pager.totalPages ? 'disabled' : ''}`}>
+                            <Link onClick={submitHandler} to={{ search: `?page=${pager.currentPage + 1}` }} className="page-link">Next</Link>
+                        </li>
+                        <li className={`page-item last-item ${pager.currentPage === pager.totalPages ? 'disabled' : ''}`}>
+                            <Link onClick={submitHandler} to={{ search: `?page=${pager.totalPages}` }} className="page-link">Last</Link>
+                        </li>
+                    </ul>
+                }                    
+            </div>
+        </div>
+    );
+}
+
+export { HomePage };
---
+++ server/server.py
@@ -0,0 +1,78 @@
+from flask import Flask, render_template, request,jsonify 
+from flask_cors import CORS, cross_origin
+from store import Trie
+from pagination import paginate
+import json 
+import time 
+
+app = Flask(__name__)
+CORS(app)
+
+# Setting up the Trie
+root = Trie()
+with open('data/fake_band_names_mit.txt') as f:
+    words = f.readlines()
+
+print('Loading words')
+start = time.time()
+for word in words:
+    tokens = word.split(' ')
+    for token in tokens:
+        root.insert(token, word)
+    word.rstrip('\n')
+    
+end = time.time()
+
+print("Time in loading words: ",end - start)
+
+del words
+
+#endpoint for search
+@app.route('/api/query', methods=['GET'])
+@cross_origin()
+def search():
+    artist = request.args.get('artist').title()
+    # get page from query params or default to first page
+    page = request.args.get('page') if request.args.get('page') else 1
+    
+    # search by artist name
+    split_words = artist.split()
+    if len(split_words) == 0:
+        resp = jsonify([])
+        resp.status_code = 200
+        return resp
+        
+    last_word = split_words[-1]
+    
+    prefix = ' '.join(split_words[:-1])
+
+    suggestions = root.query(artist)
+    
+    full_suggestions = []
+
+    indx=0
+
+    for suggestion in suggestions:
+        full_suggestions.append({
+            "id": indx+1,
+            "artistName": '{}'.format(suggestion[0]).strip(),
+            "rank": suggestion[1],
+        })
+        indx = indx+1
+
+    sorted_suggestions = sorted(full_suggestions, key=lambda k: k['rank'], reverse=True)
+
+    # get pager object for specified page
+    pager = json.loads(paginate(len(sorted_suggestions), int(page)))
+
+    # get page of items from suggestions
+    pageOfItems = sorted_suggestions[pager['startIndex']: pager['endIndex']+1]
+
+    resp = jsonify({'pager':pager, 'pageOfItems': pageOfItems})
+    resp.headers.add("Access-Control-Allow-Headers", "*")
+    resp.status_code = 200
+    return resp
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run()
